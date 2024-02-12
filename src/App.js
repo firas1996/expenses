e54c5c1690099daa25bae2fd3e7b070a0193aeb2
firas hamila <firas.hm.1996@gmail.com>
2024-02-12 20:59:00 +0100

    kk

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
+import { useState } from "react";
 import AddExpenses from "./components/AddExpenses";
 import ExpenseItem from "./components/ExpenseItem";
 import ExpensesContainer from "./components/ExpensesContainer";
 
+const expensesData = [
+  {
+    id: 1,
+    title: "New TV",
+    price: 1234,
+    date: new Date(2023, 5, 7),
+  },
+  {
+    id: 2,
+    title: "Education",
+    price: 5999,
+    date: new Date(2024, 8, 17),
+  },
+  {
+    id: 3,
+    title: "Voyage",
+    price: 2500,
+    date: new Date(2023, 10, 23),
+  },
+];
 function App() {
-  const expensesData = [
-    {
-      id: 1,
-      title: "New TV",
-      price: 1234,
-      date: new Date(2023, 5, 7),
-    },
-    {
-      id: 2,
-      title: "Education",
-      price: 5999,
-      date: new Date(2024, 8, 17),
-    },
-    {
-      id: 3,
-      title: "Voyage",
-      price: 2500,
-      date: new Date(2023, 10, 23),
-    },
-  ];
+  const [expenses, setExpenses] = useState(expensesData);
+  const getData = (data) => {
+    setExpenses((prevState) => {
+      return [data, ...expenses];
+    });
+  };
+  console.log(expenses);
   return (
     <div>
-      <AddExpenses />
-      <ExpensesContainer expensesData={expensesData} />
+      <AddExpenses getData={getData} />
+      <ExpensesContainer expensesData={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 const ExpenseItem = ({ title, price, date }) => {
-  const [newTitle, setNewTitle] = useState(title);
-  const updateTitle = () => {
-    console.log(newTitle);
-    setNewTitle("abc");
-    console.log(newTitle);
-  };
+  // const [newTitle, setNewTitle] = useState(title);
+  // const updateTitle = () => {
+  //   console.log(newTitle);
+  //   setNewTitle("abc");
+  //   console.log(newTitle);
+  // };
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{newTitle}</h2>
-        <button onClick={updateTitle}>Update Title</button>
+        <h2>{title}</h2>
+        {/* <button onClick={updateTitle}>Update Title</button> */}
         <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
